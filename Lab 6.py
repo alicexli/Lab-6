71805ba6000a134eb2ddba6454484f1220f434c9
Hannah-Dixon <Hannah-Dixon@github.com>
2024-10-23 20:03:58 -0400

    Decoder is working. Encoder was updated. A couple formatting things where changed in the print function.

--- Lab 6.py
+++ Lab 6.py
@@ -1,7 +1,12 @@
 def encoder(password):
+  digit = 0
   new_password = ''
-  for i in range(8):
-    new_password += int(password[i,i+1] + 3)
+  for i in range(len(password)):
+    digit = int(password[i]) + 3
+    if int(digit) > 9:
+      new_password += str(digit - 10)
+    else:
+      new_password += str(digit)
   return new_password
 
 # decodes the password
@@ -15,10 +20,12 @@ def decode(encoded_password):
         letter += 10
       letter = str(letter)
       password += letter
+  return password
 
 password = ''
 option = 0
 while option != 3:
+  print("")
   print("Menu \n------------- \n1. Encode \n2. Decode \n3. Quit")
   option = int(input("Please enter an option: "))
   if option == 1:
@@ -27,6 +34,7 @@ while option != 3:
     print("Your password has been encoded and stored!")
   elif option == 2:
     password = decode(encoded_password)
-    print("The encoded password is", encoder(password), "and the original password is", password, ".")
+    print("The encoded password is", encoded_password, "and the original password is", password, end = ".")
+    print("")
   else:
     quit
